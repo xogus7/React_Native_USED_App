e1ef5e4b7d4bc452ea272ac692a4c1b5e0a928d2
xogus7 <xogus2676@gmail.com>
2024-07-24 13:51:39 +0900

    chore: 절대경로 설정

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@components': './src/components',
+          '@icons': './src/assets/icons',
+        },
+      },
+    ],
+  ],
 };
