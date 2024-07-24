e764ef02a662d64cde12726b0e6ee6cc904ffa55
xogus7 <xogus2676@gmail.com>
2024-07-24 13:53:30 +0900

    feat: 검색화면

---
+++ src/pages/Search.jsx
@@ -0,0 +1,86 @@
+import React, {useState} from 'react';
+import {
+  FlatList,
+  SafeAreaView,
+  Text,
+  View,
+  TextInput,
+  Image,
+  TouchableOpacity,
+  useWindowDimensions,
+} from 'react-native';
+
+import BasicHeader from '@components/BasicHeader';
+
+
+const Search = ({ navigation, route }) => {
+  const [query, setQuery] = useState('');
+  const itemList = route.params;
+  const { width } = useWindowDimensions();
+  const columns = 3;
+  const imageSize = width / columns - 24;
+
+  const searchedItemList = itemList.filter(e => e.product.title.includes(query));
+
+  const renderItem = ({item}) => {
+    return (
+      <TouchableOpacity
+        onPress={() => navigation.navigate('Product', {item})}
+        style={{
+          paddingVertical: 8,
+          paddingHorizontal: 8,
+          borderColor: '#e2e2e2',
+          borderWidth: 1,
+          borderRadius: 8,
+        }}>
+        <View style={{}}>
+          <Image
+            style={{ width: imageSize, height:  imageSize+ 50, borderRadius: 8}}
+            source={{uri: item.productImagesUrl[0]}}
+          />
+          <View style={{}}>
+            <Text style={{color: '#000', fontSize: 16, fontWeight: 'bold'}}>
+              {item.product.title}
+            </Text>
+            <Text style={{color: '#000', fontWeight: 'bold'}}>
+              {item.product.price.toLocaleString()}원
+            </Text>
+            <Text style={{color: '#999'}}>
+              {item.location.dong}
+            </Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <SafeAreaView style={{flex: 1, backgroundColor: '#fff'}}>
+      <BasicHeader component={
+        <TextInput
+        placeholder="검색어를 입력해주세요."
+        style={{
+          flex: 1,
+          marginLeft: 16,
+          paddingHorizontal: 16,
+          borderRadius: 8,
+          borderWidth: 1,
+          borderColor: '#eeeeee',
+          color: 'black',
+        }}
+        onChangeText={setQuery}
+        />
+      } 
+      rightButtonHide={true}/>
+      
+      <FlatList
+        style={{paddingHorizontal: 8}}
+        data={searchedItemList}
+        renderItem={renderItem}
+        numColumns={columns}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default Search;
