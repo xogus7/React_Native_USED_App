1dd49a6d5822dfb360da50563d0e5b88b1165b65
xogus7 <xogus2676@gmail.com>
2024-07-26 03:26:47 +0900

    refactor: image import문 변경

--- src/components/BottomTab.jsx
+++ src/components/BottomTab.jsx
@@ -1,15 +1,8 @@
 import React, { useRef } from 'react';
 import { View, TouchableOpacity, Animated, StyleSheet  } from 'react-native';
+import { home_fill, home_empty, search_fill, search_empty,
+    add_off, chat_fill, chat_empty, mypage_fill, mypage_empty } from '@icons/bottomtab'
 
-const home_fill = require('@icons/bottomtab/home_fill.png');
-const home_empty = require('@icons/bottomtab/home_empty.png');
-const search_fill = require('@icons/bottomtab/search_fill.png');
-const search_empty = require('@icons/bottomtab/search_empty.png');
-const add_off = require('@icons/bottomtab/add_circle_off.png');
-const chat_fill = require('@icons/bottomtab/chat_fill.png');
-const chat_empty = require('@icons/bottomtab/chat_empty.png');
-const mypage_fill = require('@icons/bottomtab/mypage_fill.png');
-const mypage_empty = require('@icons/bottomtab/mypage_empty.png');
 
 const BottomTab = ({ state, navigation, insets, descriptors }) => {
     const tab1Value = useRef(new Animated.Value(0)).current;
